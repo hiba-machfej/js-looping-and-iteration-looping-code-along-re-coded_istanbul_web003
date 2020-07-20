b355308490dab22204320cf18095f094fb1de2d5
Hiba Machfej <hiba.mashfij@gmail.com>
2020-07-20 20:11:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const event = 'birthday';
 
 function writeCards(birhdayNames, event) {
   for (let i = 0; i < birhdayNames.length; i++) {
-    console.log(`Thank you, ${birhdayNames[i]} , for the wonderful ${event} gift!`);
+    console.log(`Thank you, ${birhdayNames} , for the wonderful ${event} gift!`);
   }
 
 }
