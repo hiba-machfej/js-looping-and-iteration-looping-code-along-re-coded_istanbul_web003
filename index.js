5a0087fc8058c3b97078532b52ff159f8515dcd8
Hiba Machfej <hiba.mashfij@gmail.com>
2020-07-20 20:10:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+let birhdayNames = ["Ada", "Brendan", "Ali"];
+let event = 'birthday';
+
+function writeCards(birhdayNames, event) {
+  for (let i = 0; i < birhdayNames.length; i++) {
+    console.log(`Thank you, ${birhdayNames[i]} , for the wonderful ${event} gift!`);
+  }
+
+}
+ 
+
+writeCards(birhdayNames, event)
